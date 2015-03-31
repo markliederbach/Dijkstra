a6e759db24d583a26fdd04a6c6cca9840d6baeb6
Mark Liederbach <contact@markliederbach.com>
2015-03-31 17:33:36 -0500

    Added Dijkstra Java package

---
+++ Dijkstra/Dijkstra.java
@@ -0,0 +1,175 @@
+package Dijkstra;
+
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.concurrent.atomic.AtomicLong;
+
+public interface Dijkstra {
+	
+	static final AtomicLong NEXT_ID = new AtomicLong(0);
+	
+	
+	public class Node implements Comparable<Node>{
+
+		public String name;
+		public final long id;
+		public ArrayList<Edge> neighbors = new ArrayList<Edge>();
+		public double minDistance = Double.POSITIVE_INFINITY;
+		public Node previous;
+		
+		public Node(String name){
+			this.name = name;
+			this.id = NEXT_ID.getAndIncrement();	
+		}
+		public String toString(){
+			return this.name;
+		}
+		public ArrayList<String> stringNeighbors(){
+			ArrayList<String> neighbors = new ArrayList<String>();
+			for(Edge e: this.neighbors){
+				neighbors.add(e.toNode.toString());
+			}
+			return neighbors;
+		}
+		public int compareTo(Node arg0) {
+			//Returns: 0 if equal, <0 if arg0 is greater, >0 if arg0 is lesser
+			return Double.compare(this.minDistance, arg0.minDistance);
+		}
+		public boolean addNeighbor(Node neigh, double weight){
+			if(this.neighbors != null){
+			for(Edge n: this.neighbors){
+				if(n.toNode.id == neigh.id){
+					return false;
+			}
+			}
+			}
+				this.neighbors.add(new Edge(neigh,weight));
+				return true;
+			
+			
+		}
+		
+	}//End Node Class
+	
+	
+	
+	public class Edge{
+		public final Node toNode;
+		public final double weight;	//Distance from parent source node to toNode
+		
+		public Edge(Node toNode, double weight){
+			this.toNode = toNode;
+			this.weight = weight;
+		}
+	}//End Edge Class
+	
+	public class RunDijkstra{//Allows multiple path computations from a set source
+		public final ArrayList<Node> nodes;	//ArrayList of Nodes (each contain their own ArrayList of neighbors)
+		public Node source;
+		
+		public RunDijkstra(ArrayList<Node> nodes, Node source){
+			this.nodes = nodes;
+			
+			//check that source is in map
+			for(Node n: this.nodes){
+				if(source.id == n.id)
+					this.source = source;
+			}
+			//if not, display error
+			if(this.source.id != source.id){
+				System.err.println("Source node is not in the map!");
+				System.exit(0);
+			}
+			
+		}//End constructor
+		
+		public boolean changeSource(Node newSource){//not working
+			for(Node n: this.nodes){
+				if(newSource.id == n.id)
+					this.source = newSource;
+			}
+			//if not, display error
+			if(this.source.id != newSource.id){
+				System.out.println("New source node is not in the map!");
+				return false;
+			}
+			return true;
+		}//End changeSource
+		
+		public ArrayList<Node> findPath(Node target){
+			if(!this.nodes.contains(target)){
+				System.out.println("Target node is not in the map!");
+				return null;
+			}
+			
+			ArrayList<Node> result = new ArrayList<Node>();
+			ArrayList<Node> localList = new ArrayList<Node>();
+			
+			//Initialization
+			//load all nodes to local list
+			for(Node n: this.nodes){
+				localList.add(n);
+				//reset all distances to infinity
+				n.minDistance = Double.POSITIVE_INFINITY;
+			}
+
+			//set source dist to 0
+			source.minDistance = 0;
+			
+			//Loop
+			while(localList.size()>0){
+				//load node in localList with next minimum distance
+				//and remove it from localList
+				Collections.sort(localList);
+				Node curr = localList.remove(0);
+				
+				//for all neighbors of curr
+				for(Edge n: curr.neighbors){
+					
+					double dist = curr.minDistance + n.weight;
+					if(dist < n.toNode.minDistance){
+						n.toNode.minDistance = dist;
+						n.toNode.previous = curr;
+					}
+					
+				}
+				if(curr.id == target.id){
+					break;
+				}
+				
+			}
+			
+			//We now have all information to construct the path from source to target
+			Node tempTarget = target;
+			while(tempTarget.previous != null){
+				result.add(tempTarget);
+				tempTarget = tempTarget.previous;
+			}
+			result.add(source);
+			Collections.reverse(result);
+			
+			flushData();
+			
+			if(result.size()>1){
+			return result;
+			}else{
+				return null;
+			}
+			
+			
+			
+		}
+		
+		private void flushData(){
+			for(Node n: this.nodes){
+				n.minDistance = Double.POSITIVE_INFINITY;
+				n.previous = null;
+			}
+		}
+		
+		
+	}//End RunDijkstra
+	
+	
+}//End interface
+
---
+++ Dijkstra/TestDriver.java
@@ -0,0 +1,116 @@
+package Dijkstra;
+
+import java.util.ArrayList;
+
+public class TestDriver implements Dijkstra{
+	
+	/*
+	 * Basic steps to using Dijkstra interface:
+	 * 		1. Create your nodes
+	 * 		2. Create Edges between nodes by adding neighbor and weight to a node
+	 * 			a. If undirected, you need to create edges from BOTH sides. Else it's only one way.
+	 * 		3. Add Edges to 
+	 * 		4. Create RunDijkstra object
+	 * 		5. Assuming you've set source to desired node, call findPath method to return ArrayList of path (in order)
+	 * 		6. Source can be changed at any time.
+	 * 		
+	 * 
+	 * 
+	 */
+
+	
+	//Good example on webpage: http://improve.dk/generic-dijkstras-algorithm/
+	
+	//Example Run
+	public static void main(String[] arg){
+	//Testing dijkstra's algorithm interface
+	//Create Nodes
+	Node U = new Node("U");
+	Node X = new Node("X");
+	Node W = new Node("W");
+	Node V = new Node("V");
+	Node Y = new Node("Y");
+	Node Z = new Node("Z");
+	
+	//Create edges
+	U.addNeighbor(X, 5);
+	U.addNeighbor(W, 3);
+	U.addNeighbor(V, 7);
+	
+	X.addNeighbor(U, 5);
+	X.addNeighbor(W, 4);
+	X.addNeighbor(Y, 7);
+	X.addNeighbor(Z, 9);
+	
+	W.addNeighbor(U, 3);
+	W.addNeighbor(V, 3);
+	W.addNeighbor(Y, 8);
+	W.addNeighbor(X, 4);
+	
+	V.addNeighbor(U, 7);
+	V.addNeighbor(Y, 4);
+	V.addNeighbor(W, 3);
+	
+	Y.addNeighbor(X, 7);
+	Y.addNeighbor(W, 8);
+	Y.addNeighbor(V, 4);
+	Y.addNeighbor(Z, 2);
+	
+	Z.addNeighbor(X, 9);
+	Z.addNeighbor(Y, 2);
+	
+	
+	//Now that all nodes are defined and have relationships to other nodes, make an ArrayList
+	ArrayList<Node> map = new ArrayList<Node>();
+	map.add(U);
+	map.add(X);
+	map.add(W);
+	map.add(V);
+	map.add(Y);
+	map.add(Z);
+	
+	//We will use Node U as our initial source
+	
+	//Initialize RunDijkstra class
+	RunDijkstra test = new RunDijkstra(map,U);
+	
+	//Find path from U to target: Z
+	
+	ArrayList<Node> result = test.findPath(Z);
+	
+	//Print result
+	printNodeArrayList(result);
+	
+	
+	
+	//Change source to Z
+	test.changeSource(map.get(5)); //Another way of changing source
+	
+	//Find route from Z to target: W
+	printNodeArrayList(test.findPath(W));
+	
+	
+	//Change to source NOT in map
+	Node newNode = new Node("M");
+	
+	//Attempt to find path from M (will not run)
+	test.findPath(newNode);
+	
+	//Attempt to change source to M (even though it's not in the map)
+	System.out.println(test.changeSource(newNode));//returns false
+	
+	}//End main
+
+	public static void printNodeArrayList(ArrayList<Node> in){
+		if(in != null){
+		for(Node n: in){
+			System.out.print(n.toString() + " ");
+		}
+		System.out.println();
+		}else{
+			System.out.println("No route found...");
+		}
+	}
+	
+	
+}
